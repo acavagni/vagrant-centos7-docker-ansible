c1073b2ce85f2294e70473de2708d5009ff98f51
Alessandro Cavagni <alessandro.cavagni@gmail.com>
2018-08-25 23:29:07 +0200

    added tmux and strace packages

--- provision/ansible_docker/01-root-install-utils.sh
+++ provision/ansible_docker/01-root-install-utils.sh
@@ -2,4 +2,4 @@
 
 echo "Executing [01-root-install-utils.sh] ..."
 
-yum -y install tree sshpass git psmisc vim-enhanced python-virtualenv python-beautifulsoup4 python2-pip tcpdump nmap nmon pstree jq yum-utils
+yum -y install tree sshpass git psmisc vim-enhanced python-virtualenv python-beautifulsoup4 python2-pip tcpdump nmap nmon pstree jq yum-utils strace tmux
